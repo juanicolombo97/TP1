277c8db46b2ebb8317915ac913a55e62901e5652
JUani <juanicolombo@icloud.com>
2020-10-20 17:44:49 -0300

    cambios sercom estilo codigo

--- common_socket.c
+++ common_socket.c
@@ -11,20 +11,23 @@ int socket_create(socket_t *self, struct sockaddr_in server) {
 }
 
 int socket_connect(socket_t *self) {
-    if (connect(self->socket, (struct sockaddr *)&self->server, sizeof(self->server)) < 0) {
+    struct sockaddr *s_sock = (struct sockaddr *)&self->server;
+    if (connect(self->socket,s_sock , sizeof(self->server)) < 0) {
         printf("Error conexion");
     }
     return 0;
 }
 
 int socket_bind_and_listen(socket_t *self) {
-   if( bind(self->socket,(struct sockaddr *)&self->server , sizeof(self->server)) < 0) {
-       printf("BINd failed");
-   }
-   listen(self->socket, 10);    
-   socklen_t client_size = sizeof(self->client);
-   self->new_socket =  accept(self->socket, (struct sockaddr *)&self->client,&client_size);
-
+     struct sockaddr *s_sock = (struct sockaddr *)&self->server;
+     struct sockaddr *c_sock = (struct sockaddr *)&self->client;
+    if( bind(self->socket,s_sock, sizeof(self->server)) < 0) {
+        printf("BINd failed");
+    }
+    listen(self->socket, 10);    
+    socklen_t client_size = sizeof(self->client);
+    self->new_socket =  accept(self->socket, c_sock,&client_size);
+    return self->new_socket;
 }
 
 int socket_send(socket_t *self, const char* buffer, size_t lenght) {
